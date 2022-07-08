1271371084ef6c956e44090a29e695566367b461
ab7nt <85429584+ab7nt@users.noreply.github.com>
2022-07-08 15:23:43 +0300

    change design and add player

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,37 @@
 (() => {
-  const parallaxFrame = document.querySelector('.parallax-frame');
-  const parallaxWrapperLeft = document.querySelector('.parallax-wrapper-left');
-  const parallaxWrapperRight = document.querySelector('.parallax-wrapper-right');
+  function parralaxBackground() {
+    const parallaxBg = document.querySelector('.mouse-parallax-bg');
 
-  window.addEventListener('mousemove', (ev) => {
-    let x = ev.clientX / window.innerWidth * 50;
-    let y = ev.clientY / window.innerHeight * 50;
+    window.addEventListener('mousemove', (ev) => {
+      let x = ev.clientX / window.innerWidth;
+      let y = ev.clientY / window.innerHeight;
+  
+      parallaxBg.style.transform = `translate(-${Math.round(x * 50)}px, -${Math.round(y * 50)}px)`
+    })
+  }
+  parralaxBackground();
 
-    parallaxWrapperLeft.style.transform = `translate(${-x + 50}px, ${-y + 50}px)`
-    parallaxWrapperRight.style.transform = `translate(${x - 15}px, ${y - 350}px)`
-  })
+  function liveLine() {
+    const playerInfo = document.querySelector('.player-info');
+    let trackName = 'Автор - Название песни.'.split('');
+    let latency = 50;
+
+    trackName.forEach(el => {
+      setTimeout(function() {
+        playerInfo.textContent += el;
+      }, latency)
+      latency += 50
+    })
+  }
+  liveLine();
+
+  function renderVolumeSteps() {
+    const volumeSteps = document.querySelector('.volume-steps');
+
+    for(i = 0; i < 10; i++) {
+      volumeSteps.innerHTML += `
+      <div class="volume-step"></div>`
+    }
+  }
+  renderVolumeSteps()
 })();
